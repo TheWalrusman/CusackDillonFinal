c8c40a963c93a5aa1e3837d7bca3b323a0cbf968
Kyle Cusack <kdc6w3@mst.edu>
2018-05-06 07:13:27 -0500

    documented and fixed CenterDiffMesh

--- PDEsolver/CenterDiffMesh.hpp
+++ PDEsolver/CenterDiffMesh.hpp
@@ -16,22 +16,33 @@ template< class T, class U, class V, T xLower(const U, const V), T xUpper(const 
 void CenterDiffMesh<T, U, V, xLower, xUpper, yLower, yUpper>::operator()(symmetricMatrix<T>& A, Math_Vector<T>& B)
 {
 
-	//unsigned m_subDivisions = atoi(argv[1]);
+
 	double h = 1.0 / (m_subDivisions);
 	double onefourth = 1.0 / 4.0;
+
 	double x = 1;
+
 	double y = 1;
 	double xp;
+
 	double xm;
+
 	double yp;
+
 	double ym;
+
 	double bSum;
+
 	bool firstOnBound;
+	bool secOnBound;
 	bool thirdOnBound;
+	bool fourthOnBound;
+
 	symmetricMatrix<double> correctA((m_subDivisions - 1)*(m_subDivisions - 1));
 	A = correctA;
 	B.resize((m_subDivisions - 1)*(m_subDivisions - 1));
 	Math_Vector<double> nb;
+
 	nb.resize((m_subDivisions - 1)*(m_subDivisions - 1));
 
 
@@ -42,112 +53,144 @@ void CenterDiffMesh<T, U, V, xLower, xUpper, yLower, yUpper>::operator()(symmetr
 
 		//===== GENERATE THE PARAM VALUES USED IN EQUATION ======
 		xp = x + 1;
+
 		xm = x - 1;
+
 		yp = y + 1;
+
 		ym = y - 1;
+
 		//====================================================== 
 
 
 		//============ ASSUME NO TERMS ARE ON BOUNDARY AT FIRST =====
 		firstOnBound = false;
+		secOnBound = false;
 		thirdOnBound = false;
+		fourthOnBound = false;
 		//======================================================
 
 
 		//================= GENERATE THE NUMBER THAT NEEDS TO GO INTO THE B VECTOR =========
+		// We do not need to look at the upper and right location because of the banded and symmetric
+		//nature of the matrix we only need to look at the lower and left locations to fill out the matrix
 		bSum = 0;
+
 		//see first term
 		if (xm == 0)
 		{
-			//nbSum += xLower(nxm*h, newy*h);
+
 			bSum += xLower(xm*h, y*h);
 			firstOnBound = true;
 		}
 		if (xm == m_subDivisions)
 		{
-			//nbSum += xUpper(nxm*h, newy*h);
+
 			bSum += xUpper(xm*h, y*h);
 			firstOnBound = true;
 		}
 		//see second term
 		if (xp == 0)
 		{
-			//nbSum += xLower(nxp*h, newy*h);
+
 			bSum += xLower(xp*h, y*h);
+			secOnBound = true;
 		}
 		if (xp == m_subDivisions)
 		{
-			//nbSum += xUpper(nxp*h, newy*h);
+
 			bSum += xUpper(xp*h, y*h);
+			secOnBound = true;
 		}
+
 		//see third term
 		if (ym == 0)
 		{
-			//nbSum += yLower(newx*h, nym*h);
+
 			bSum += yLower(x*h, ym*h);
 			thirdOnBound = true;
 		}
 		if (ym == m_subDivisions)
 		{
-			//nbSum += yUpper(newx*h, nym*h);
+
 			bSum += yUpper(x*h, ym*h);
 			thirdOnBound = true;
 		}
 		//see fourth term
 		if (yp == 0)
 		{
-			//nbSum += yLower(newx*h, nyp*h);
+
 			bSum += yLower(x*h, yp*h);
+			fourthOnBound = true;
 		}
 		if (yp == m_subDivisions)
 		{
-			//nbSum += yUpper(newx*h, nyp*h);
+
 			bSum += yUpper(x*h, yp*h);
+			fourthOnBound = true;
 		}
 
 		B[i] = bSum;
+
 		//===================== END GENERATE B VALUE ===========================
 
 
 		//=================== FILL IN MATRIX =======================
 		//mapping equation -----> col = ( ((y/h)-1) * (m_subDivisions-1) ) + (x/h) - 1 
-		A.getRef(i,i) = 1; //diagonal is always 1
+		A.getRef(i, i) = 1; //diagonal is always 1
+
 		unsigned col;
+
 		if (!firstOnBound)
 		{
 			//xm   y
 			col = static_cast<unsigned>((((y)-1) * (m_subDivisions - 1)) + (xm)-1);
-			A.getRef(i,col) = -onefourth;
+			A.getRef(i, col) = -onefourth;
+
 		}
+
 		if (!thirdOnBound)
 		{
 			//x   ym
 			col = static_cast<unsigned>((((ym)-1) * (m_subDivisions - 1)) + (x)-1);
 			A.getRef(i, col) = -onefourth;
+
 		}
+
 		//=============================================================
 
 
 
 		//======================== UPDATE X AND Y ============================
-		//update "x" and "y" as needed
-		//cout << "(x,y) = " << "(" << x << "," << y << ")" << "     i = " << i << endl;
-		//cout << "(newx,newy) = " << "(" << newx * h << "," << newy * h << ")" << "     i = " << i << endl;
+
 		x += 1;
+
 		if (x == m_subDivisions)
 		{
 			x = 1;
 			y += 1;
+
 		}
 		//========================================================================
 
 	}
 
+	cout << secOnBound << fourthOnBound << endl;
+
+
 
 	B = B * onefourth;
 
-	return;
+
+
 }
+
+
+
+
+
+
+
 
 
 
